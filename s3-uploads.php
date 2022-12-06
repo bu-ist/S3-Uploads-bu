90e650d9e34942947e56971c702f9bd0878b2aeb
Jonathan Williams <tamlin@mac.com>
2022-12-06 16:57:33 -0500

    move autoload inside the plugin
    in order to make the plugin self sufficient.  The readme indicates that the autoloader should be called from `wp-config.php`, but we load it here instead to make the plugin more portable. Here is the relevant line in the readme:
    https://github.com/humanmade/S3-Uploads/blob/a15fccd4d63599caec6c45ec200113a1db0737a9/README.md?plain=1#L49

--- s3-uploads.php
+++ s3-uploads.php
@@ -12,6 +12,11 @@ require_once __DIR__ . '/inc/namespace.php';
 
 add_action( 'plugins_loaded', 'S3_Uploads\\init', 0 );
 
+if ( ! class_exists( '\\Aws\\S3\\S3Client' ) ) {
+	// Require AWS Autoloader file.
+   require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+}
+
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	WP_CLI::add_command( 's3-uploads', 'S3_Uploads\\WP_CLI_Command' );
 }
